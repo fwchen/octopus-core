6b3f3d5cc6f74d9982cd4d7bcbd6f1ccc0e948f8
chchen <chchen@thoughtworks.com>
2020-05-14 11:36:17 +0800

    initial commit

---
+++ src/main/kotlin/com/chenfangwei/octopus/core/OctopusCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.chenfangwei.octopus.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OctopusCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<OctopusCoreApplication>(*args)
+}
