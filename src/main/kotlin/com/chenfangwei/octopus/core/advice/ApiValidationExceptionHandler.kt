d51f1611534b22433fba0b53b72d6f446a88821d
fwchen <me@chenfangwei.com>
2020-05-27 23:18:21 +0800

    add ApiValidationExceptionHandler

---
+++ src/main/kotlin/com/chenfangwei/octopus/core/advice/ApiValidationExceptionHandler.kt
@@ -0,0 +1,20 @@
+package com.chenfangwei.octopus.core.advice
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class ApiValidationExceptionHandler : ResponseEntityExceptionHandler() {
+  override fun handleMethodArgumentNotValid(
+          ex: MethodArgumentNotValidException,
+          headers: HttpHeaders,
+          status: HttpStatus,
+          request: WebRequest): ResponseEntity<Any> {
+    return ResponseEntity(HttpStatus.BAD_REQUEST)
+  }
+}
--- src/main/kotlin/com/chenfangwei/octopus/core/domain/project/command/CreateProjectCommand.kt
+++ src/main/kotlin/com/chenfangwei/octopus/core/domain/project/command/CreateProjectCommand.kt
@@ -1,5 +1,3 @@
 package com.chenfangwei.octopus.core.domain.project.command
 
-data class CreateProjectCommand(val identify: String, val name: String, var creatorId: String) {
-
-}
+data class CreateProjectCommand(val identify: String, val name: String, var creatorId: String) {}
