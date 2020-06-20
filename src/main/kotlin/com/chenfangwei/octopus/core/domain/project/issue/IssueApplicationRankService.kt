f0fcb36e47a0f0414665fdcb25e9305f21dc22ae
fwchen <me@chenfangwei.com>
2020-06-21 00:18:45 +0800

    update

--- src/main/kotlin/com/chenfangwei/octopus/core/domain/project/issue/IssueApplicationRankService.kt
+++ src/main/kotlin/com/chenfangwei/octopus/core/domain/project/issue/IssueApplicationRankService.kt
@@ -12,14 +12,16 @@ class IssueApplicationRankService(private val issueRepository: IssueRepository) 
         val changedIssues = ArrayList<Issue>()
         val targetIssue = issueRepository.findById(targetIssueId).orElseThrow{ EntityNotFoundException() }
         val issue = issueRepository.findById(issueId).orElseThrow{ EntityNotFoundException() }
-        val nextIssue: Issue? = if (isBefore) issueRepository.findFirstByProjectIdAndOrderLessThan(issue.projectId, targetIssue.order) else issueRepository.findFirstByProjectIdAndOrderGreaterThanOrderByOrderAsc(issue.projectId, targetIssue.order)
+        val nextIssue: Issue? = if (isBefore) issueRepository.findFirstByProjectIdAndOrderLessThanOrderByOrderDesc(issue.projectId, targetIssue.order) else issueRepository.findFirstByProjectIdAndOrderGreaterThanOrderByOrderAsc(issue.projectId, targetIssue.order)
         if (nextIssue == null) {
             issue.order = targetIssue.order - if (isBefore) 100F else -100F
             changedIssues.add(issue)
             return changedIssues
         }
-        if (kotlin.math.abs(targetIssue.order - nextIssue.order) < 100) {
-            changedIssues.addAll(adjustIssueOrder(if (isBefore) targetIssue else nextIssue, 50F, isBefore))
+        if (kotlin.math.abs(targetIssue.order - nextIssue.order) < 20F) {
+//            var adjustIssue = if (isBefore) targetIssue else nextIssue
+//            var baseIssue = if (isBefore) nextIssue else targetIssue
+            changedIssues.addAll(adjustIssueOrder(nextIssue, targetIssue.order, 100F, isBefore))
         }
         issue.order = targetIssue.order + (if (isBefore) -1 else 1) * kotlin.math.abs(targetIssue.order - nextIssue.order) / 2
         changedIssues.add(issue)
@@ -27,15 +29,23 @@ class IssueApplicationRankService(private val issueRepository: IssueRepository) 
         return changedIssues
     }
 
-    private fun adjustIssueOrder(issue: Issue, baseOrder: Float, isBefore: Boolean): List<Issue> {
+    private fun adjustIssueOrder(issue: Issue, baseOrder: Float, adjustNumber: Float, isBefore: Boolean): List<Issue> {
+        val realAdjustNumber = if (isBefore) -adjustNumber else adjustNumber
         val changedIssues = ArrayList<Issue>()
-        val nextIssue = if (isBefore) issueRepository.findFirstByProjectIdAndOrderLessThan(issue.projectId, issue.order) else issueRepository.findFirstByProjectIdAndOrderGreaterThanOrderByOrderAsc(issue.projectId, issue.order)
-        if (nextIssue == null || kotlin.math.abs(nextIssue.order - issue.order) >= 2 * baseOrder) {
-            issue.order += if (isBefore) -baseOrder else baseOrder
+        val nextIssue = if (isBefore) issueRepository.findFirstByProjectIdAndOrderLessThanOrderByOrderDesc(issue.projectId, issue.order) else issueRepository.findFirstByProjectIdAndOrderGreaterThanOrderByOrderAsc(issue.projectId, issue.order)
+        if (nextIssue == null) {
+            issue.order += realAdjustNumber
             changedIssues.add(issue)
+            return changedIssues
+        }
+        val diff = if (isBefore) issue.order - nextIssue.order else nextIssue.order - issue.order
+        if (diff >= 2 * adjustNumber) {
+            issue.order  = baseOrder +  realAdjustNumber
+            changedIssues.add(issue)
+           return changedIssues
         } else {
-            changedIssues.addAll(adjustIssueOrder(nextIssue, baseOrder, isBefore))
-            issue.order += if (isBefore) -baseOrder else baseOrder
+            changedIssues.addAll(adjustIssueOrder(nextIssue, baseOrder + realAdjustNumber, adjustNumber, isBefore))
+            issue.order = baseOrder + realAdjustNumber
             changedIssues.add(issue)
         }
         return changedIssues
